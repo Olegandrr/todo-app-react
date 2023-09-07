ae7ad98490a89dcc0ea8979603728eda119a899e
Oleg <olegandrr@gmail.com>
2023-09-07 10:56:45 +0300

    3.2.11

--- src/components/NewTaskForm/NewTaskForm.jsx
+++ src/components/NewTaskForm/NewTaskForm.jsx
@@ -1,72 +1,63 @@
 /* eslint-disable jsx-a11y/no-autofocus */
 import PropTypes from 'prop-types'
-import { Component } from 'react'
+import { useState, useRef, useEffect } from 'react'
 
-class NewTaskForm extends Component {
-  state = {
-    inputValue: { newTask: '', minutes: '', seconds: '' },
-  }
+function NewTaskForm({ handleInputKeyDown }) {
+  const [inputValue, setInputValue] = useState({ newTask: '', minutes: '', seconds: '' })
+  const { newTask, minutes, seconds } = inputValue
+  const newTaskRef = useRef()
 
-  handleInputChange = (e) => {
+  useEffect(() => {
+    newTaskRef.current.focus()
+  }, [newTask])
+
+  const handleInputChange = (e) => {
     const { name, value } = e.target
-    const { inputValue } = this.state
-    this.setState({ inputValue: { ...inputValue, [name]: Math.abs(value) || value } })
+    setInputValue({ ...inputValue, [name]: Math.abs(value) || value })
   }
-
-  handleKeyDown = (e) => {
-    const {
-      inputValue: { newTask, minutes, seconds },
-    } = this.state
-    const { handleInputKeyDown } = this.props
+  const handleKeyDown = (e) => {
     if (e.key === 'Enter' && String(newTask).trim().length !== 0) {
       e.preventDefault()
       const validMinutes = parseInt(minutes, 10) || 0
       const validSeconds = parseInt(seconds, 10) || 0
       const userTimerSeconds = validMinutes * 60 + validSeconds
       handleInputKeyDown(newTask, userTimerSeconds)
-      this.setState({ inputValue: { newTask: '', minutes: '', seconds: '' } })
+      setInputValue({ newTask: '', minutes: '', seconds: '' })
     }
   }
-
-  render() {
-    const {
-      inputValue: { newTask, minutes, seconds },
-    } = this.state
-    return (
-      <form className="new-todo-form">
-        <input
-          value={newTask}
-          className="new-todo"
-          placeholder="Task"
-          type="text"
-          onChange={this.handleInputChange}
-          onKeyDown={this.handleKeyDown}
-          name="newTask"
-          autoFocus
-        />
-        <input
-          value={minutes}
-          className="new-todo-form__timer"
-          placeholder="Min"
-          name="minutes"
-          onChange={this.handleInputChange}
-          onKeyDown={this.handleKeyDown}
-          type="number"
-        />
-        <input
-          value={seconds}
-          className="new-todo-form__timer"
-          placeholder="Sec"
-          name="seconds"
-          onChange={this.handleInputChange}
-          onKeyDown={this.handleKeyDown}
-          type="number"
-          max="59"
-          title="Введите значение от 0 до 59"
-        />
-      </form>
-    )
-  }
+  return (
+    <form className="new-todo-form">
+      <input
+        value={newTask}
+        className="new-todo"
+        placeholder="Task"
+        type="text"
+        onChange={handleInputChange}
+        onKeyDown={handleKeyDown}
+        name="newTask"
+        ref={newTaskRef}
+      />
+      <input
+        value={minutes}
+        className="new-todo-form__timer"
+        placeholder="Min"
+        name="minutes"
+        onChange={handleInputChange}
+        onKeyDown={handleKeyDown}
+        type="number"
+      />
+      <input
+        value={seconds}
+        className="new-todo-form__timer"
+        placeholder="Sec"
+        name="seconds"
+        onChange={handleInputChange}
+        onKeyDown={handleKeyDown}
+        type="number"
+        title="Введите значение от 0 до 59"
+      />
+    </form>
+  )
 }
 
 NewTaskForm.propTypes = {
