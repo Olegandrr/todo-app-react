5a42d0b0b2710bc6cb1052fe2872837a45848f88
Oleg <olegandrr@gmail.com>
2023-09-07 10:14:33 +0300

    3.2.11

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,32 +1,24 @@
 import PropTypes from 'prop-types'
-import { Component } from 'react'
 
 import TasksFilter from '../TasksFilter'
 
-class Footer extends Component {
-  itemsLeft = () => {
-    const { list } = this.props
-    return list.length !== 0 ? list.filter((item) => !item[4]).length : 0
-  }
+function Footer({ list, filterComplete, filterActive, filterAll, clearCompleted }) {
+  const itemsLeft = () => (list.length !== 0 ? list.filter((item) => !item[4]).length : 0)
 
-  handleClickCompleted = () => {
-    const { list, clearCompleted } = this.props
+  const handleClickCompleted = () => {
     const clearList = list.filter((item) => !item[4])
     clearCompleted(clearList)
   }
 
-  render() {
-    const { filterComplete, filterActive, filterAll } = this.props
-    return (
-      <footer className="footer">
-        <span className="todo-count">{this.itemsLeft()} items left</span>
-        <TasksFilter filterComplete={filterComplete} filterActive={filterActive} filterAll={filterAll} />
-        <button className="clear-completed" onClick={this.handleClickCompleted} type="button">
-          Clear completed
-        </button>
-      </footer>
-    )
-  }
+  return (
+    <footer className="footer">
+      <span className="todo-count">{itemsLeft()} items left</span>
+      <TasksFilter filterComplete={filterComplete} filterActive={filterActive} filterAll={filterAll} />
+      <button className="clear-completed" onClick={handleClickCompleted} type="button">
+        Clear completed
+      </button>
+    </footer>
+  )
 }
 
 Footer.defaultProps = {
